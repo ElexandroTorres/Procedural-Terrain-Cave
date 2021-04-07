366535bd834712f075b2d8c00cb2c7ad2dc4ca26
Elexandro Torres <elexandro.torres@gmail.com>
2021-04-07 13:06:28 -0300

    Pequenos testes

---
+++ Assets/Scripts/MeshGeneration.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGeneration : MonoBehaviour
+{
+    Mesh mesh;
+
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    public int xSize = 500;
+    public int zSize = 500;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    private void CreateShape()
+    {
+        int i = 0;
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        triangles = new int[3];
+        triangles[0] = 0;
+        triangles[1] = xSize + 1;
+        triangles[2] = 1;
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+
+    void OnDrawGizmos()
+    {
+        if(vertices == null)
+            return;
+
+        
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+
+    } 
+}
