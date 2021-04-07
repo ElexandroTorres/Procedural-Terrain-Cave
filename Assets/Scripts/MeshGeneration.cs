ed45e331897b0b664f375c0ed2509b17834e9096
Elexandro Torres <elexandro.torres@gmail.com>
2021-04-07 14:29:08 -0300

    geração, com os gizmos

--- Assets/Scripts/MeshGeneration.cs
+++ Assets/Scripts/MeshGeneration.cs
@@ -17,12 +17,14 @@ public class MeshGeneration : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-
+        
         CreateShape();
         UpdateMesh();
+        
     }
 
-    private void CreateShape()
+    
+    void CreateShape()
     {
         int i = 0;
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
@@ -31,15 +33,48 @@ public class MeshGeneration : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[i] = new Vector3(x, 0, z);
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
                 i++;
             }
         }
 
-        triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = xSize + 1;
-        triangles[2] = 1;
+        triangles = new int[xSize * xSize * 6];
+
+        int index = 0;
+        int currentVertex = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[index] = currentVertex;
+                triangles[index + 1] = xSize + 1 + currentVertex;
+                triangles[index +2] = currentVertex + 1;
+
+                triangles[index +3] = currentVertex + 1;
+                triangles[index +4] = xSize + 1 + currentVertex;
+                triangles[index +5] = xSize + 2 + currentVertex; 
+
+                index += 6;
+                currentVertex++;
+
+
+                /*
+                triangles = new int[6];
+                triangles[0] = 0;
+                triangles[1] = xSize + 1;
+                triangles[2] = 1;
+                triangles[3] = 1;
+                triangles[4] = xSize + 1;
+                triangles[5] = xSize + 2;    
+                */
+            }
+            currentVertex++;
+        }
+
     }
 
     private void UpdateMesh()
